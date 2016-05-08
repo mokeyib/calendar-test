8e13ba83056ba4e27a9297c10471156708ce78d7
Ingrid Biery <ingridbiery@gmail.com>
2016-05-08 13:46:55 +0000

    use a new google account

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,23 +1,24 @@
 class Calendar
-  attr_reader :signed_in
-  SPRING_2016_PUBLIC_ID = 'a8elc993pj7mdgeq5m0knbboc4'
-  SPRING_2016_MEMBERS_ONLY_ID = '0g4ch638r3bn787bbpi2pbi5hc'
+  FIRST_ID = 'is6tkoosdic12kgm0bpsuvb0b0'
+  SECOND_ID = 'tqvb0959ftb5qdfarnc5mecqc8'
   BROWN = '%23A32929'
-  GREEN = "%2328754E"
+  GREEN = '%2328754E'
+  DARK_BLUE = '%2329527A'
+  GREEN2 = '%231B887A'
 
   def initialize
   end
 
-  def public_cal
-    "src=#{SPRING_2016_PUBLIC_ID}%40group.calendar.google.com&color=#{BROWN}&"
+  def first_cal
+    "src=#{FIRST_ID}%40group.calendar.google.com&color=#{DARK_BLUE}&"
   end
 
-  def members_only_cal
-    "src=#{SPRING_2016_MEMBERS_ONLY_ID}%40group.calendar.google.com&color=#{GREEN}&"
+  def second_cal
+    "src=#{SECOND_ID}%40group.calendar.google.com&color=#{GREEN}&"
   end
 
   def url
-    cals = "#{public_cal}#{members_only_cal}"
+    cals = "#{first_cal}#{second_cal}"
     "https://calendar.google.com/calendar/embed?title=CUCO%20Calendar&height=600&wkst=1&bgcolor=%23FFFFFF&#{cals}ctz=America%2FNew_York"
   end
 end
